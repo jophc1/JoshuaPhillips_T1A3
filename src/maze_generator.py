446a627296008c00f92e79271e746499b1a3a772
jophc1 <jphcairns@gmail.com>
2023-05-05 20:09:11 +1000

    working on maze algorithm on feature 1, up to correct path randomisation

--- src/maze_generator.py
+++ src/maze_generator.py
@@ -30,45 +30,89 @@ elif user_input_difficulty in 'hard':
     path_length += path_length
 
 # Create a path using path_length to connect the start and end point. 
-# Will need to use some randomized direction will creating path
+# Will need to use some randomized direction while creating path
 
     # from start point, decide which direction to go next on the path. This could
     # be to any random direction as long as its not where you came from and as long as 
     # this new path won't be blocked in.
 current_row = start_point   # begin at start point
 current_column = 0          #
+current_path_index = 2 #index starts at 2 as start point is 1
+restricted_direction = None
 
 # While the path hasnt reached the end
 while True:
+    #Save the last position from prior loop as varibles
+    last_row = current_row
+    last_column = current_column
+  
         # Check if we are next to an edge (left,right,top,bottom) and in a corner
-    if current_column == 0: # left edge
-        if current_row == 0: # top left corner
+    # left edge    
+    if current_column == 0: 
+        
+        # top left corner
+        if current_row == 0: 
             pass
             # next path cannot go left or up
-        elif current_row == (row_maze - 1): #bottom left corner
+
+        #bottom left corner    
+        elif current_row == (row_maze - 1): 
             pass
             #next path cannot go left or down
-        else: # left edge but not top or bottom corner
+
+        # left edge but not a corner   
+        else: 
             pass
             #next path cannot go left
-    elif current_column == (column_maze - 1): #right edge, this column has exit on it
-        if current_row == end_point: # we are on end point, end loop by breaking out of while loop
-            break
-        elif current_row == 0: #top right corner
+
+    #right edge, this column has exit on it        
+    elif current_column == (column_maze - 1): 
+
+        # end point 
+        if current_row == end_point: 
+            break #end loop by breaking out of while loop
+
+        #top right corner
+        elif current_row == 0: 
             pass
             #next path cannot go right or up
-        elif current_row == (row_maze - 1): #bottom right corner
+
+        #bottom right corner    
+        elif current_row == (row_maze - 1): 
             pass
             #next path cannot go right or down
-        else: # right edge but not top or bottom corner
-            pass
-            #next path cannot go right
-    elif current_row == 0: # top edge
-        pass
-    elif current_row == (row_maze - 1): # bottom edge
-        pass
-    else: # not next to an edge
-        pass
 
-    # After we make a new path, subtract one from path_length
-    path_length -= 1
+        # right edge but not a corner
+        else: 
+            pass
+            #next path cannot go right, but also shouldn't go either up or down depending where the end point is located otherwise path may be stuck
+
+    # top edge        
+    elif current_row == 0: 
+        pass
+        #next path cannnot go up, but also shouldn't go left as it'll block path
+
+    # bottom edge
+    elif current_row == (row_maze - 1): 
+        pass
+        #next path cannot go down, but also shouldn't go left as it'll block path
+
+    # not next to an edge or corner
+    else: 
+        pass
+    
+    # determine which direction last path came from, we can use this to restrict next path going 
+    # back in that direction (eg if path went to right, then we dont want next path going left)
+    if last_row == current_row:
+        if last_column < current_column:
+            restricted_direction = 'left'
+        else:
+            restricted_direction = 'right'
+    else:
+        if last_row < current_row:
+            restricted_direction = 'top'
+        else:
+            restricted_direction = 'bottom'
+
+    # After we make a new path, add one to current_path_index
+    current_path_index += 1
