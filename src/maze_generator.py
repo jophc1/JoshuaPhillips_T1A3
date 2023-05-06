7c0bf861d1e07ab8fa940be32c08a95006ddbdd3
jophc1 <jphcairns@gmail.com>
2023-05-06 13:47:25 +1000

    working on correct path item in maze algorithm

--- src/maze_generator.py
+++ src/maze_generator.py
@@ -15,10 +15,10 @@ end_point = random.randint(0, column_maze - 1)
 
 maze_paths[start_point][0] = 1
 maze_paths[end_point][column_maze - 1] = 1
-pprint(maze_paths)
+# pprint(maze_paths)
 
 # length of longest linear path from start to end point will be from one corner to another corner
-path_length = (column_maze - 1) + (row_maze - 1) # This is length from top left corner of maze to bottom right corner
+path_length = (column_maze - 1) + (row_maze - 1) # This is length from up left corner of maze to down right corner
 
     # add an amount to path_length depending on difficulty level
 user_input_difficulty = 'medium' # this will be from terminal prompt, remember to lowercase actual input later
@@ -40,68 +40,96 @@ current_column = 0          #
 current_path_index = 2 #index starts at 2 as start point is 1
 restricted_direction = None
 
+# create a function to check a direction that 
+
+
 # While the path hasnt reached the end
 while True:
     #Save the last position from prior loop as varibles
     last_row = current_row
     last_column = current_column
   
-        # Check if we are next to an edge (left,right,top,bottom) and in a corner
+        # Check if we are next to an edge (left,right,up,down) and in a corner
     # left edge    
     if current_column == 0: 
-        
-        # top left corner
-        if current_row == 0: 
-            pass
-            # next path cannot go left or up
-
-        #bottom left corner    
-        elif current_row == (row_maze - 1): 
-            pass
-            #next path cannot go left or down
-
+        # up left corner or down left corner
+        if current_row == 0 or current_row == (row_maze - 1): 
+            # next path cannot go left or up, also can't go right as path would be stuck if old path came from right
+            # therefore only path is to go left
+            current_column += 1
+            maze_paths[current_row][current_column] = current_path_index
         # left edge but not a corner   
         else: 
-            pass
             #next path cannot go left
+            # check where last path came from
+            if restricted_direction in 'right':
+                pass
+                #can only go up or down, but should check that there are no previous paths there
+            elif restricted_direction in 'up':
+                pass
+                # can only go down or right
+            elif restricted_direction in 'down':
+                pass
+                #can only go up or right
 
     #right edge, this column has exit on it        
     elif current_column == (column_maze - 1): 
-
         # end point 
         if current_row == end_point: 
             break #end loop by breaking out of while loop
-
-        #top right corner
+        #up right corner
         elif current_row == 0: 
-            pass
-            #next path cannot go right or up
-
-        #bottom right corner    
+            #next path cannot go right or up, also cannot go left as this would block path
+            # therefore can only go down
+            current_row += 1
+            maze_paths[current_row][current_column] = current_path_index
+        #down right corner    
         elif current_row == (row_maze - 1): 
-            pass
-            #next path cannot go right or down
-
+            #next path cannot go right or down, also cannot go left as this would block path
+            #therefore can only go up
+            current_row -= 1
+            maze_paths[current_row][current_column] = current_path_index
         # right edge but not a corner
         else: 
-            pass
             #next path cannot go right, but also shouldn't go either up or down depending where the end point is located otherwise path may be stuck
+            # above end point
+            if current_row < end_point:
+                current_row += 1
+            #below end point
+            else:
+                current_row -= 1
 
-    # top edge        
-    elif current_row == 0: 
-        pass
-        #next path cannnot go up, but also shouldn't go left as it'll block path
-
-    # bottom edge
-    elif current_row == (row_maze - 1): 
-        pass
-        #next path cannot go down, but also shouldn't go left as it'll block path
-
+    # top edge         
+    elif current_row == 0 or current_row == (row_maze - 1): 
+        #next path cannnot go up, but also shouldn't go left as it'll block path, also can't go left as that will block path
+        # therefore can only go right or down
+        # this will determine right or up/down
+        random_direction = random.randint(0, 1)
+        #if last step came from up or down, only go right
+        if restricted_direction == 'down' or restricted_direction == 'up':
+            current_column += 1
+            maze_paths[current_row][current_column] = current_path_index
+        # if top edge
+        elif current_row == 0:
+            if not random_direction:
+                current_row += 1
+            else:
+                current_column += 1
+        # if bottom edge
+        else:
+            if not random_direction:
+                current_row -= 1
+            else:
+                current_column += 1
+           
     # not next to an edge or corner
     else: 
         pass
     
-    # determine which direction last path came from, we can use this to restrict next path going 
+    # debugging, remove later: print out new maze_paths
+    pprint(maze_paths)
+
+    # determine which direction last path came from, we can use this to restrict the next path going 
     # back in that direction (eg if path went to right, then we dont want next path going left)
     if last_row == current_row:
         if last_column < current_column:
@@ -110,9 +138,12 @@ while True:
             restricted_direction = 'right'
     else:
         if last_row < current_row:
-            restricted_direction = 'top'
+            restricted_direction = 'up'
         else:
-            restricted_direction = 'bottom'
+            restricted_direction = 'down'
 
     # After we make a new path, add one to current_path_index
     current_path_index += 1
+
+    # do the logic for cases not next to edges
+
