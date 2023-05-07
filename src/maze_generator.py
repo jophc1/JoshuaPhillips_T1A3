018b682a69baab3dd1d3d067c354a9483a0a9a26
jophc1 <jphcairns@gmail.com>
2023-05-07 19:12:38 +1000

    finished fake path generation, needs refactoring correct path code

--- src/maze_generator.py
+++ src/maze_generator.py
@@ -1,6 +1,54 @@
 import random
 from pprint import pprint
 
+def restricted_move(prior_coordinates: tuple, current_coordinates: tuple) -> str:
+        if prior_coordinates[0] == current_coordinates[0]:
+            if prior_coordinates[1] < current_coordinates[1]:
+                direction = 'left'
+            else:
+                direction = 'right'
+        else:
+            if prior_coordinates[0] < current_coordinates[0]:
+                direction = 'up'
+            else:
+                direction = 'down'
+        return direction
+
+def current_moves(row_position: int, column_position: int, maze_paths_indexes: list, restrict_left: int = 1) -> list:
+    valid_directions = []
+    #Check left
+    if column_position -1 >= 0 and not maze_paths_indexes[row_position][column_position - 1] and not restrict_left:
+        valid_directions.append('left')
+    #Check up
+    if row_position - 1 >= 0 and not maze_paths_indexes[row_position - 1][column_position]:
+        valid_directions.append('up')
+    #Check right
+    if column_position + 1 <= (len(maze_paths_indexes[0]) - 1) and not maze_paths_indexes[row_position][column_position + 1]:
+        valid_directions.append('right')
+    #Check down
+    if row_position + 1 <= (len(maze_paths_indexes) - 1) and not maze_paths_indexes[row_position + 1][column_position]:
+        valid_directions.append('down')
+    #Check if all directions blocked
+    if not len(valid_directions): # all direction blocks
+        valid_directions.append('blocked')
+    #return list of directions in could go in as a string list e.g ['left', 'up', 'down']
+    return valid_directions
+
+def assign_new_path_index(row_position: int, column_position: int, direction_list: list, current_index: int, maze_list: list):
+    move_direction = direction_list[random.randint(0, len(direction_list) - 1)]
+
+    if move_direction in 'left':
+        column_position -= 1
+    elif move_direction in 'up':
+        row_position -= 1
+    elif move_direction in 'right':
+        column_position += 1
+    else: #down
+        row_position += 1
+    maze_list[row_position][column_position] = current_index
+    return row_position, column_position
+
+
 # create a maze the size of provided parameters in rows and columns
 # lets make row and column be 10 eg. 10x10 maze
 row_maze = 10
@@ -16,18 +64,6 @@ end_point = random.randint(0, row_maze - 1)
 maze_paths[start_point][0] = 1
 maze_paths[end_point][column_maze - 1] = 1
 # pprint(maze_paths)
-
-#obselete code - old difficulty level 
-    # # length of longest linear path from start to end point will be from one corner to another corner
-    # path_length = (column_maze - 1) + (row_maze - 1) # This is length from up left corner of maze to down right corner
-    #     # add an amount to path_length depending on difficulty level
-    # user_input_difficulty = 'medium' # this will be from terminal prompt, remember to lowercase actual input later
-    # if user_input_difficulty in 'easy':
-    #     path_length += (path_length - (path_length // 2))
-    # elif user_input_difficulty in 'medium':
-    #     path_length += (path_length - (path_length // 4))
-    # elif user_input_difficulty in 'hard':
-    #     path_length += path_length
 
 # Create a path using path_length to connect the start and end point. 
 # Will need to use some randomized direction while creating path
@@ -164,35 +200,78 @@ while True:
 
     # determine which direction last path came from, we can use this to restrict the next path going 
     # back in that direction (eg if path went to right, then we dont want next path going left)
-    if last_row == current_row:
-        if last_column < current_column:
-            restricted_direction = 'left'
-        else:
-            restricted_direction = 'right'
-    else:
-        if last_row < current_row:
-            restricted_direction = 'up'
-        else:
-            restricted_direction = 'down'
+
+    restricted_direction = restricted_move((last_row, last_column), (current_row, current_column))
 
     # After we make a new path, add one to current_path_index
     current_path_index += 1
 
 # debugging, remove later: print out new maze_paths
 pprint(maze_paths)
+end_point_index = current_path_index - 1 # this is used for evaluating the end point when creating ascii maze
 
 # generate fake paths around correct path
 
-# easy mode does 3 fake paths
-# medium does 4 fake paths
-# hard does 5 fake paths
+# easy mode has a miniumum of 2 fake paths + amount of columns of maze // 6
+# medium does 2 fake paths + amount of columns of maze // 5
+# hard does 2 fake paths + amount of columns of maze // 4
 
-# one fake path will be from 0-2 position of start point
-# another fake path will be from 0-2 positions from end point
+user_input_difficulty = 'hard' # this will be from terminal prompt, remember to lowercase actual input later
+if user_input_difficulty in 'easy':
+    fake_modifier = 6
+elif user_input_difficulty in 'medium':
+    fake_modifier = 5
+else:
+    fake_modifier = 4
+
+fake_path_quantity = random.randint(column_maze // fake_modifier, (column_maze // fake_modifier) + 2)
+
+# one fake path will be from 1-3 position of start point
+# another fake path will be from 1-3 positions from end point
 # all other fake paths will be from positions between these two fake paths starting points
+fake_path_start = random.randint(1,3)
+fake_path_end = random.randint(current_path_index - 3, current_path_index - 1)
+fake_path_set = set([fake_path_start, fake_path_end])
+
+while len(fake_path_set) < fake_path_quantity + 2:
+    fake_path_set.add(random.randint(fake_path_start + 1, fake_path_end - 1))
+
+fake_path_starting_points = list(fake_path_set)
+fake_path_starting_points.sort()
+print(fake_path_starting_points)
 
 # Find starting point of fake path, evaluate if there can be a fake path generated from correct path
-# if not find another fake path starting point (maybe try position next to it) else we can generate a fake path from it
+# if not continue to next fake path starting point else we can generate a fake path from it
+correct_to_fake_connection = [] # this will contain a list of tuples that will have the indexes of correct path index and first step of fake path
+for fake_starting_point in fake_path_starting_points:
+    # find where this point is on correct path and get the row, column coordinates
+    # find row and column
+    for row_index in range(len(maze_paths)):
+        if fake_starting_point in maze_paths[row_index]:
+            current_row = row_index
+            current_column = maze_paths[row_index].index(fake_starting_point)
+            break
+    print(current_row, current_column)
+
+    # need a way to save first path index from fake path and associate it with correct path index so we can print out a clear spot for fake path
+    next_path_direction = ""
+    while True:
+        # check if there is a valid path next to current point (i.e is there a spot left,right,up or down that is 0)
+        current_path_index += 1 
+        next_path_direction = current_moves(current_row, current_column, maze_paths, 0)
+        if "blocked" in next_path_direction :
+            break
+        current_row, current_column = assign_new_path_index(current_row, current_column, next_path_direction, current_path_index, maze_paths)
+        correct_to_fake_connection.append((fake_starting_point, current_path_index))
+
+pprint(maze_paths)
+
+
+        
+
+
+
+
 
 # Now we start generating fake path, everytime checking where our last fake path came from (restricted_direction)
 # and checking if we can move a certain direction.
